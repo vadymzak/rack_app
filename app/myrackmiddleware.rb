0305b7954e0e7643e83d72ca97c192d9db722e1e
Vadym Zakharchenko <vadim.if@gmail.com>
2018-11-16 12:49:45 +0200

    check token from header and params

--- app/myrackmiddleware.rb
+++ app/myrackmiddleware.rb
@@ -11,40 +11,51 @@ class MyRackMiddleware
   def call(env)
     @env = env
     request(env)
-    #p @env
-
-    status, @headers, body = @appl.call(env) # we now call the inner application
-    p @headers
-    get_token(@request.GET)
-    append_s = check_token
-    status = 401 if append_s == "Token not valid"
-    [status, @headers, body << append_s.to_s]
+    status, headers, body = @appl.call(env) # we now call the inner application
+    status = set_status
+    @append_s = set_body(status)
+    [status, headers, body << @append_s.to_s]
   end
 
   def request(env)
     @request = Rack::Request.new(env)
   end
 
-  def get_token(params)
-    @token = ""
-    params.each { |key, value| @token = value if key == "token"}
+  def bearer_token
+    @bearer_token = nil
+    @bearer_token ||= authorization_header || @request.GET['token']
   end
 
-  def check_token
-    check = Check.new(@token.to_s, get_key)
-    unless check.decode_HS256 == false
-      return check.decode
+  def set_body(status)
+    if status == 200
+      return @check.decode
     else
       return "Token not valid"
     end
+  end
+
+  private
+
+  def check_token?
+    bearer_token
+    @check = Check.new(@bearer_token.to_s, get_key)
+    @check.decode_HS256
   end
 
   def get_key
     key = 'my$ecretK3y'
   end
 
-  def get_header
-    binding.pry
-    request.headers['Authorization']
+  def authorization_header
+    @env['HTTP_TOKEN']
   end
+
+  def check_status?
+    check_token?
+  end
+
+  def set_status
+    check_status?() ? 200 : 401
+  end
+
 end
